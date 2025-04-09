6cc7f8a603b4a47f3289f6a8a4d433634ab2cac6
JDtArD <justsayoos@gmail.com>
2025-04-09 14:51:15 -0400

    Create LIST.py

---
+++ LIST.py
@@ -0,0 +1,6 @@
+#pedir user, 3 numeros y hacer una lista con esos 3 numeros
+Num = int(input("Ingrese un numero\n"))
+Num2 = int(input("Otro mas\n"))
+Num3 = int(input("Ultimo mas\n"))
+ListN = (Num, Num2, Num3)
+print (f"Sus numeros son:{ListN}")
